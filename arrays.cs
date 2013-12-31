789b1507f22ef3e387139f934f600de48e809f69
Geeza <geza.csorba@barre.hu>
2013-12-31 22:54:37 +0100

    not complete yet

--- arrays.cs
+++ arrays.cs
@@ -8,9 +8,14 @@ namespace arrays
 {
     class Program
     {
+        //int score = 0;
+        public int ARRAY_LENGTH = 10000;
+
         static void Main(string[] args)
         {
-            new Program().task1();
+            new Program().task2();
+
+            Console.ReadLine();
         }
 
         bool correctAnswer(string answer)
@@ -49,7 +54,7 @@ namespace arrays
             Console.WriteLine(codeForTask1);
             int answer1 = -1;
             try { answer1 = Convert.ToInt32(Console.ReadLine()); }
-            catch (Exception e) { Console.WriteLine("for real? no input dude...");/* hehe */ }
+            catch (Exception e) { Console.WriteLine("for real? no input dude...");/* hehe */ e.ToString(); }
 
             int cAnswer = -1 ;
             int[] array0 = new int[5];
@@ -64,10 +69,130 @@ namespace arrays
             {
                hasMoreTries = (cAnswer == answer1 ) ? correctAnswer(Convert.ToString(answer1)) : fail(Convert.ToString(answer1), Convert.ToString(cAnswer),ref tries);
                try { answer1 = Convert.ToInt32(Console.ReadLine()); }
-               catch (Exception e) { Console.WriteLine("for real? no input dude...");/* hehe */ }
+               catch (Exception e) { Console.WriteLine("for real? no input dude...");/* hehe */ e.ToString(); }
             }
 
-            answer1 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void changeValues(ref int a,ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        public void displayArray(int[] arrToDisplay)
+        {
+            foreach (int i in arrToDisplay)
+            {
+                Console.Write("[{0}] ", i);
+            }
+            Console.WriteLine("");
+        }
+
+        // buborékrendezés
+        public void magicSort(ref int[] array1, ref int changedCntr)
+        {
+            bool changed=true;
+            while (changed)
+            {
+                changed = false;
+                for (int i = 0; i + 1 < array1.Length; i++)
+                {
+                    if (array1[i] > array1[i + 1])
+                    {
+                        changeValues(ref array1[i], ref array1[i + 1]);
+                        changed = true;
+                        changedCntr++;
+                    }
+                }
+            }
+        }
+
+        public void DoGezaSort(ref int[] array1, ref int changedCntr)
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; i + j < array1.Length; j++)
+                {
+                    if (array1[i] > array1[i + j])
+                    {
+                        changedCntr++;
+                        changeValues(ref array1[i], ref array1[i + j]);
+                    }
+                }
+            }
+        }
+
+        public void DonaldShellSort(ref int[] arr, ref int changedCntr)
+        {
+            int i, j, k, tmp, num;
+            num = arr.Length;
+            for (i = num / 2; i > 0; i = i / 2)
+            {
+                for (j = i; j < num; j++)
+                {
+                    for (k = j - i; k >= 0; k = k - i)
+                    {
+                        if (arr[k + i] >= arr[k])
+                            break;
+                        else
+                        {
+                            tmp = arr[k];
+                            arr[k] = arr[k + i];
+                            arr[k + i] = tmp;
+                            changedCntr++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void task2()
+        {
+            ARRAY_LENGTH = 5;
+            int[] array1 = new int[ARRAY_LENGTH];
+
+            string arrElements = "The current elements of the array: ";
+            
+
+            Random r = new Random();
+            int cnt = 0;
+            
+            foreach(int i in array1){
+                array1[cnt++] = r.Next(0,ARRAY_LENGTH);
+            }
+            int[] array2 = (int[])array1.Clone();
+            int[] array3 = (int[])array1.Clone();
+
+            int changedCntr=0;
+            int changedCntr2=0;
+            int changedCntr3=0;
+
+            //Console.WriteLine(arrElements);
+            //displayArray(array1);
+
+            string intro = "Let's see the effectiveness of sorting algorithms. Effectiveness is about time, and time depends on swap count. Therefore, the better algorithm " +
+                "makes less swaps, so it finishes earlier on the same machine. Take a look at the following array: ";
+            string intro2 = "how many swaps does Bubble sort need to sort it?";
+            
+            Console.WriteLine(intro);
+            displayArray(array1);
+            Console.WriteLine(intro2);
+
+
+            DoGezaSort(ref array1, ref changedCntr);
+            magicSort(ref array2, ref changedCntr2);
+            DonaldShellSort(ref array3, ref changedCntr3);
+
+            //Console.WriteLine("*after sort*");
+            //Console.WriteLine(arrElements);
+            //displayArray(array1);
+            Console.WriteLine("my own sort: {0} times occured swap, \tswaps/arraysize={1}", changedCntr, changedCntr/ARRAY_LENGTH);
+            Console.WriteLine("Bubble: {0} times occured swap, \tswaps/arraysize={1}", changedCntr2, changedCntr2 / ARRAY_LENGTH);
+            Console.WriteLine("Shell: {0} times occured swap, \tswaps/arraysize={1}", changedCntr3, changedCntr3 / ARRAY_LENGTH);
+
+            Console.WriteLine("\nResults: Shell algorithm could execute the sort {0} times, while Bubble finishes only one.", changedCntr2/changedCntr3 );
         }
     }
 }
